5f9a7419bca8b3776088e2c7e196c4ec313d9ddf
UrazbakhtinIV <urazbakhtiniv@gmail.com>
2022-10-09 22:28:12 +0300

    Тестирование scss

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+        <div className='wrapperApp'>
+            <h1>Test</h1>
+        </div>
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style/normalize.css';
 import './index.css';
-import App from './App';
+import App from './components/app/App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <App/>
   </React.StrictMode>
 );
 
